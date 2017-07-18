bafd561ff5b38d1acb6bf6d4a45a280805eb0a19
xing <xing@xin.gl>
2017-07-18 01:14:11 -0700

    DLVA Charts App

---
+++ index.js
@@ -0,0 +1,74 @@
+
+var airtable = require("airtable") , 
+	express = require("express") , 
+	bodyParser = require("body-parser");
+
+airtable.configure({
+	apiKey: "key9rX4SfkIdPvMjd"
+})
+var  base = airtable.base('appiOTKmVcjI5OBZ6');
+
+/*
+	Calls Airtable API to get charts for airport
+*/
+function get_chart(airport, callback) {
+    var results;
+	base('SID/STAR Charts').select({
+	    // Selecting the first 3 records in Grid view:
+	    maxRecords: 1,
+	    view: "Grid view",
+	    filterByFormula : "{Airport} = '"+airport+"'"
+	}).eachPage(function page(records, fetchNextPage) {
+	    if(records.length === 0) {
+	    	results = "Charts for airport "+airport+" not found. Check name and try again."
+	    } else {
+		    records.forEach(function(record) {
+		        results = record.get('Charts');
+		    });
+		}
+	    // To fetch the next page of records, call `fetchNextPage`.
+	    // If there are more records, `page` will get called again.
+	    // If there are no more records, `done` will get called.
+	    fetchNextPage();
+
+	}, function done(err) {
+
+	    if (err) { 
+	    	callback("Sorry, somethings broken :-(")
+	    } else {
+	    	callback(results)
+	    }
+	});
+};
+var app = express();
+
+
+app.post("/",bodyParser.urlencoded(),function(req,res){
+
+	var airport = req.body.text ;
+	if(airport && typeof(airport) == "string" && (airport = airport.trim()).length ===4 ) {
+		get_chart(airport.toUpperCase(), function(result){
+			res.setHeader('Content-Type', 'application/json');
+			res.send(JSON.stringify({
+				"response_type"  : "ephemeral" , 
+				"text" : "Charts for " + airport.toUpperCase() ,
+				"attachments" : [
+					{"text" : result }
+				]
+			}));
+
+		})
+	}
+	else {
+		res.setHeader('Content-Type', 'application/json');
+		res.send(JSON.stringify({
+			"response_type"  : "ephemeral" , 
+			"text" : "Charts command expects a 4 letter airport code",
+			"attachments" : [
+				{"text" : airport + " does not pass the sniff test" }
+			]
+		}));
+	}
+})
+
+app.listen(8005)
---
+++ test.js
@@ -0,0 +1,59 @@
+var querystring = require('querystring') , 
+    http = require("http")
+     , assert = require('assert') ;
+
+
+function mockSlackRequest(airport, callback) {
+	var post_data = querystring.stringify({
+		command : "/charts",
+		text : airport
+	})
+  var post_options = {
+      host: 'localhost',
+      port: '8005',
+      path: '',
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Content-Length': Buffer.byteLength(post_data)
+      }
+  };
+  var post_req = http.request(post_options, function(res){
+  	  res.setEncoding("utf8")
+  	  var result = '';
+  	  res.on("data",function(chunk){
+  	  	 result += chunk;
+  	  })
+
+  	  res.on("end",function(){
+  	  	callback(result)
+  	  })
+  });
+  post_req.write(post_data)
+  post_req.end();
+}
+
+describe("test server functionality",function() {
+	it("test valid airport" , function(done){
+		mockSlackRequest("KLAX", function(result){
+			var obj = JSON.parse(result);
+			assert.equal("Charts for KLAX", obj.text);
+			done()
+		});
+
+	})
+	it("test invalid airport string", function(done){
+		mockSlackRequest("f",function(result){
+			var obj = JSON.parse(result)
+			assert.equal("Charts command expects a 4 letter airport code", obj.text)
+			done()
+		})
+	})
+	it("test airport not found", function(done){
+		mockSlackRequest("ddne",function(result){
+			var obj = JSON.parse(result)
+			assert.equal('Charts for airport DDNE not found.',obj.attachments[0].text)
+			done()
+		})
+	})
+});
