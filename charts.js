d213b968f45831842909aadcb63b5ee891a96874
xing <xing@xin.gl>
2017-07-18 09:32:09 -0700

    refactor post callback

--- charts.js
+++ charts.js
@@ -9,7 +9,7 @@ module.exports = function(apiKey, baseid) {
 	/*
 		Calls Airtable API to get charts url for airport
 	*/
-	return function (airport, callback) {
+	function get_chart(airport, callback) {
 	    var results;
 		base('SID/STAR Charts').select({
 		    // Selecting the first 3 records in Grid view:
@@ -38,4 +38,37 @@ module.exports = function(apiKey, baseid) {
 		    }
 		});
 	};
+    return function(req,res){
+
+		var airport = req.body.text ;
+		if(airport && typeof(airport) === "string" && (airport = airport.trim().toUpperCase()).length === 4 ) {
+			/*
+				Airport code is required to be a four letter character string
+			*/
+			get_chart(airport, function(result){
+				res.setHeader('Content-Type', 'application/json');
+				res.send(JSON.stringify({
+					"response_type"  : "ephemeral" , 
+					"text" : "Charts for " + airport ,
+					"attachments" : [
+						{"text" : result }
+					]
+				}));
+
+			})
+		}
+		else {
+			/*
+				Airport code provided is not valid
+			*/
+			res.setHeader('Content-Type', 'application/json');
+			res.send(JSON.stringify({
+				"response_type"  : "ephemeral" , 
+				"text" : "Charts command expects a 4 letter airport code",
+				"attachments" : [
+					{"text" : "Airport code" + airport + " does not pass the sniff test" }
+				]
+			}));
+		}
+	}
 };
--- index.js
+++ index.js
@@ -4,46 +4,12 @@ var charts = require("./charts.js") ,
 	bodyParser = require("body-parser") , 
 	env = require("./env.js") ;
 
-console.log(env)
-var get_chart = charts(env.apiKey, env.baseid);
 
 var app = express();
 
 app.set('port', (process.env.PORT || process.argv[2] || 5000))
 
-app.post("/charts",bodyParser.urlencoded(),function(req,res){
-
-	var airport = req.body.text ;
-	if(airport && typeof(airport) === "string" && (airport = airport.trim().toUpperCase()).length === 4 ) {
-		/*
-			Airport code is required to be a four letter character string
-		*/
-		get_chart(airport, function(result){
-			res.setHeader('Content-Type', 'application/json');
-			res.send(JSON.stringify({
-				"response_type"  : "ephemeral" , 
-				"text" : "Charts for " + airport ,
-				"attachments" : [
-					{"text" : result }
-				]
-			}));
-
-		})
-	}
-	else {
-		/*
-			Airport code provided is not valid
-		*/
-		res.setHeader('Content-Type', 'application/json');
-		res.send(JSON.stringify({
-			"response_type"  : "ephemeral" , 
-			"text" : "Charts command expects a 4 letter airport code",
-			"attachments" : [
-				{"text" : "Airport code" + airport + " does not pass the sniff test" }
-			]
-		}));
-	}
-})
+app.post("/charts",bodyParser.urlencoded(),charts(env.apiKey, env.baseid))
 
 app.get("/",function(req,res){
 	res.setHeader('Content-Type', 'application/json');
